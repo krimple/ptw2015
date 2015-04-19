bafc68454e87c3df49b150c1a29d53d68f02d9c5
Ken Rimple <krimple@chariotsolutions.com>
2015-04-19 09:40:59 -0400

    integrate firebase api

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,14 @@
 (function() {
   'use strict';
-   angular.module('demoApp', ['ngMaterial'])
-     .constant('VIEW_LOCATION', './views/')
+   angular.module('demoApp', ['ngRoute', 'ngMaterial', 'firebase'])
+     .constant('VIEW_LOCATION', 'views/')
      .config(function($routeProvider, VIEW_LOCATION) {
        $routeProvider
          .when('/', {
            templateUrl: VIEW_LOCATION + 'default.html'
+         })
+         .when('/chat', {
+           templateUrl: VIEW_LOCATION + 'chat.html'
          })
          .otherwise({
            redirectTo: '/'
